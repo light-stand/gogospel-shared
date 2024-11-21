46368d4d9361e36d2bda246d043c9b3add1e8abe
Pedro Gardim <me@pedrogardim.com>
2024-11-21 22:50:57 +0100

    feature: load user store on api init

--- common/context/ApiContext.tsx
+++ common/context/ApiContext.tsx
@@ -1,7 +1,9 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState, useEffect, useCallback } from "react";
 
 import { useAuthInit } from "@/auth/hooks/useAuthInit";
 import { ApiConnection, initializeApiConnection } from "@/interface/api";
+import { useUserStore } from "@/user/store/useUserStore";
+import { User } from "@/user/domain/User";
 
 const ApiContext = createContext({} as ApiConnection);
 
@@ -9,9 +11,21 @@ export const useApi = () => useContext(ApiContext);
 
 export const ApiClientProvider = ({ children }: { children: React.ReactElement }) => {
   const [apiConnection, setApiConnection] = useState({} as ApiConnection);
+  const { setUser } = useUserStore();
+
+  const init = useCallback(async () => {
+    const connection = initializeApiConnection();
+    const { data } = await connection.client.auth.getUser();
+    const user: User = {
+      id: data.user?.id,
+      email: data.user?.email,
+    };
+    setUser(user);
+    setApiConnection(connection);
+  }, []);
 
   useEffect(() => {
-    setApiConnection(initializeApiConnection());
+    init();
   }, []);
 
   useAuthInit(apiConnection);
